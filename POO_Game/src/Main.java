511991aa81ca30f63b9fdcc9b3bd147a6475db13
Janni0698 <janninaromo@gmail.com>
2022-12-07 21:05:38 -0500

    Se agrego comentario en archico Main.Java

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //"editado por Jannina Rodriguez"
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
